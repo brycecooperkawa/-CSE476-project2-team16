97fb1a7b2f6ce535bd96a47a8e89bd12f8366c50
brycecooperkawa <coope445@msu.edu>
2023-03-19 16:36:23 -0400

    Added game start button on click function that will eventually pass to the GameBoardActivity/View class

--- app/src/main/java/edu/msu/CSE476_team16/proj1/MainActivity.java
+++ app/src/main/java/edu/msu/CSE476_team16/proj1/MainActivity.java
@@ -2,13 +2,46 @@ package edu.msu.CSE476_team16.proj1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String GREEN_NAME = "MainActivity.nameOfGreen";
+    private final static String WHITE_NAME = "MainActivity.nameOfWhite";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView playerOneText = (TextView)this.findViewById(R.id.playerOneEditor);
+        TextView playerTwoText = (TextView)this.findViewById(R.id.playerTwoEditor);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            playerOneText.setText(extras.getString(GREEN_NAME));
+            playerTwoText.setText(extras.getString(WHITE_NAME));
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+    }
+
+    /**
+     * Handles the start game button
+     * @param view the view
+     */
+    public void onStartGame(View view) {
+        TextView playerOneText = (TextView)this.findViewById(R.id.playerOneEditor);
+        TextView playerTwoText = (TextView)this.findViewById(R.id.playerTwoEditor);
+
+        //starts the GameBoardActivity
+        Intent intent = new Intent(this, GameBoardActivity.class);
+        intent.putExtra(GREEN_NAME, playerOneText.getText().toString());
+        intent.putExtra(WHITE_NAME, playerTwoText.getText().toString());
+        startActivity(intent);
     }
 }
